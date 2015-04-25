f73c36d185ce354753d56daa73ea14b7dda6bcb7
F. Gabriel Gosselin <gabriel@evidens.ca>
2015-04-25 10:53:50 -0700

    #1: Adding Missing test fixture

---
+++ test/server/fixtures/components/TestOldComponent.jsx
@@ -0,0 +1,13 @@
+"use strict";
+import React from 'react';
+
+
+const TestOldComponent = React.createClass({
+  render() {
+    return (
+      <p>I'm a test</p>
+    );
+  }
+});
+
+export default TestOldComponent;
